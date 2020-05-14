a505a8cf4e31aa3aa9f8f38b79b6a085e171a04d
chinazhonghao <1825547767@qq.com>
2020-05-14 19:12:26 +0800

    slow fast point“

--- java/CircularArrayLoop_457.java
+++ java/CircularArrayLoop_457.java
@@ -1,5 +1,38 @@
 class Solution {
+    private int[] nums;
+
     public boolean circularArrayLoop(int[] nums) {
-        // 看不懂题目
+        this.nums = nums;
+        int slow = 0, fast = 0;
+        boolean[] flag = new boolean[nums.length];
+        for(int i=0; i<nums.length; i++){
+            if(flag[i]){
+                continue;
+            }
+            slow = fast = i;
+
+            while(nums[next(slow)]*nums[next(fast)]>0 && nums[next(slow)]*nums[next(next(fast))]>0){
+                if(flag[slow]){
+                    break;
+                }
+                flag[slow] = true;
+                slow = next(slow);
+                fast = next(next(fast));
+                if(slow == fast){
+                    if(slow != next(slow)){
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    private int next(int base){
+        base += this.nums[base];
+        while(base < 0){
+            base += this.nums.length;
+        }
+        return base % this.nums.length;
     }
 }
--- java/ImageOverlap_835.java
+++ java/ImageOverlap_835.java
@@ -1,5 +1,32 @@
 class Solution {
     public int largestOverlap(int[][] A, int[][] B) {
-        
+        Map<String, Integer> map = new HashMap<>();
+        String format = "%d-%d";
+        for(int i=0; i<A.length; i++){
+            for(int j=0; j<A[0].length; j++){
+                if(A[i][j] == 0){
+                    continue;
+                }
+                for(int m=0; m<B.length; m++){
+                    for(int n=0; n<B[0].length; n++){
+                        if(B[m][n] == 0){
+                            continue;
+                        }
+                        String key = String.format(format, i-m, j-n);
+                        if(map.containsKey(key)){
+                            map.put(key, map.get(key)+1);
+                        }
+                        else {
+                            map.put(key, 1);
+                        }
+                    }
+                }
+            }
+        }
+        int max = 0;
+        for(int v: map.values()){
+            max = Math.max(max, v);
+        }
+        return max;
     }
 }
