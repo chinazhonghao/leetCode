6bf6adabc1fd083d6b349005d201e671d44f7599
chinazhonghao <1825547767@qq.com>
2020-09-20 12:18:28 +0800

    contest 3 problems

--- WeeklyContest/20200920/5521.java
+++ WeeklyContest/20200920/5521.java
@@ -2,43 +2,46 @@ class Solution {
     public int maxProductPath(int[][] grid) {
         int rows = grid.length;
         int cols = grid[0].length;
-        int max = Integer.MIN_VALUE;
-        int[] currentResult = new int[2];
         int MAX = 1000000007;
-        Set<Integer> current = new HashSet<>();
-        Set<Integer> down = new HashSet<>();
-        Set<Integer> right = new HashSet<>();
-        for(int i=rows; i>0; i--){
-            for(int j=cols; j>0; j--){
-                int current = grid[i-1][j-1];
-                if(current == 0){
-                    result[i-1][j-1][0] = 0;
-                    result[i-1][j-1][1] = 0;
+        Node[] current = null;
+        Node[] prev = null;
+        for(int i=0; i<rows; i++){
+            prev = current;
+            current = new Node[cols];
+            for(int j=0; j<cols; j++){
+                long d = (long)grid[i][j];
+                current[j] = new Node();
+                if(i > 0){
+                    Set<Long> data = prev[j].data;
+                    for(long item: data){
+                        current[j].data.add(item * d);
+                    }
                 }
-                else {
-                    currentResult[0] = Integer.MIN_VALUE;
-                    currentResult[0] = Integer.MIN_VALUE;
-                    if(i < rows){
-                        int[] _result = result[i][j-1];
-                        if(current > 0){
-                            currentResult[0] = (_result[0] * current) % MAX;
-                            currentResult[1] = (_result[1] * current) % MAX;
-                        }
-                        else {
-                            currentResult[0] = (_result[1] * current) % MAX;
-                            currentResult[1] = (_result[0] * current) % MAX;
-                        }
-                    }
 
-                    if(j < cols){
-                        int[] _result = result[i-1][j];
-                        if(current > 0){
-                            currentResult[0] =
-                        }
+                if(j > 0){
+                    Set<Long> data = current[j-1].data;
+                    for(long item: data){
+                        current[j].data.add(item * d);
                     }
+                }
+
+                if(i == 0 && j==0){
+                    current[j].data.add(d);
                 }
             }
         }
-        return result[0][0];
+        long max = -1;
+        Node target = current[cols-1];
+        for(long item: target.data){
+           max = Math.max(item, max);
+        }
+        return (int)(max % MAX);
     }
+}
+
+class Node {
+    public Node(){
+        data = new HashSet<>();
+    }
+    public Set<Long> data;
 }
