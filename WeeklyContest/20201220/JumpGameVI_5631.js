0974e230d501f492b76d3b303511b075711f8c1f
zhonghao <zhonghao@tencent.com>
2020-12-22 09:11:42 +0800

    slider window for hard, pass one cycle

--- WeeklyContest/20201220/JumpGameVI_5631.js
+++ WeeklyContest/20201220/JumpGameVI_5631.js
@@ -4,48 +4,17 @@
  * @return {number}
  */
 var maxResult = function(nums, k) {
-    if(nums.length === 1){
-        return nums[0];
-    }
-    var start = 0, exit = false, max = nums[0];
-    while(!exit){
-        var current = 0, begin=start+1, end=0, last=0, minMaxIndex=start+1, minMax = nums[minMaxIndex];
-        if(start+k < nums.length){
-            end = start + k
+    var max = 0, record = [];
+    for(var i=nums.length-1; i>=0; i--){
+        max = nums[i] + (record.length > 0?nums[record[0]]:0);
+        while(record.length > 0 && max > nums[record[record.length-1]]){
+            record.pop();
         }
-        else {
-            end = nums.length-1;
+        record.push(i);
+        if(record.length > 0 && i+k<=record[0]){
+            record.shift();
         }
-        
-        if(end === nums.length - 1){
-            exit = true;
-        }
-        for(var i=begin; i<=end; i++){
-            if(nums[i] >= 0){
-                current += nums[i];
-                last = i;
-            }
-            if(minMax <= nums[i]){
-                minMax = nums[i];
-                minMaxIndex = i;
-            }
-        }
-        if(minMax < 0){
-            start = minMaxIndex;
-            if(exit && minMaxIndex !== nums.length-1){
-                max += nums[nums.length-1];
-            }
-            else {
-                max += minMax;
-            }
-        }
-        else {
-            start = last;
-            max += current;
-            if(exit && nums[nums.length - 1] < 0){
-                max += nums[nums.length-1];
-            }
-        }
+        nums[i] = max;
     }
     return max;
 };
