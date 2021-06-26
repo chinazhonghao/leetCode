96509dc417232f61dca0179c2cfe4a1efb21e4f0
chinazhonghao <1825547767@qq.com>
2021-06-26 17:05:57 +0800

    recover binary tree done

--- js/RecoverBinarySearchTree_99.js
+++ js/RecoverBinarySearchTree_99.js
@@ -12,9 +12,64 @@
  */
 var recoverTree = function (root) {
     let o = null, l = null, r = null;
+    function findMax(root) {
+        const stack = [];
+        let max = root;
+        while(root !== null || stack.length > 0) {
+            if(root !== null) {
+                stack.push(root);
+                root = root.left;
+            } else {
+                root = stack.pop();
+                if (root.val > max.val) {
+                    max = root;
+                }
+                root = root.right;
+            }
+        }
+        return max;
+    }
+    function findMin(root) {
+        const stack = [];
+        let min = root;
+        while(root !== null || stack.length > 0) {
+            if(root !== null) {
+                stack.push(root);
+                root = root.left;
+            } else {
+                root = stack.pop();
+                if (root.val < min.val) {
+                    min = root;
+                }
+                root = root.right;
+            }
+        }
+        return min;
+    }
     function _recursive(root) {
-        const val = root.val;
-        
+        if(root === null) {
+            return;
+        }
+        let left = null;
+        if(root.left !== null) {
+            left = findMax(root.left);
+        }
+        let right = null;
+        if(root.right !== null) {
+            right = findMin(root.right);
+        }
+        if(left !== null && left.val > root.val) {
+            l = left;
+        }
+        if (right !== null && right.val < root.val) {
+            r = right;
+        }
+        if((l !== null || r !== null) && o === null) {
+            o = root;
+            return;
+        }
+        _recursive(root.left);
+        _recursive(root.right);
     }
     _recursive(root);
     function swap(a, b) {
@@ -22,11 +77,11 @@ var recoverTree = function (root) {
         a.val = b.val;
         b.val = val;
     }
-    if(state.left !== null && state.right !== null) {
-        swap(state.left, state.right);
-    } else if(state.left !== null) {
-        swap(state.left, state.root);
+    if(l !== null && r !== null) {
+        swap(l, r);
+    } else if(l !== null) {
+        swap(l, o);
     } else {
-        swap(state.root, state.right);
+        swap(r, o);
     }
 };
